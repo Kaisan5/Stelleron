69b94f8f244eae5f3741f0e3f7f73158da7702eb
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-04-16 06:23:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,14 +9,14 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
-        abt_msg = f"<b>⟦★⟧ Hi There {mention}</b>!💫\n<b>┏━━━━━━━❪❂❫━━━━━━━━</b>\n' \
-          f'◈ <b>ᴄʀᴇᴀᴛᴏʀ</b>: <b><a href="https://t.me/rai_yan_00">Rᴀɪ Yᴀɴ</a></b>\n' \
-          f'◈ <b>ꜰᴏᴜɴᴅᴇʀ ᴏꜰ</b>: <b><a href="https://t.me/Ani_Weebs">ᴀɴɪᴍᴇ ᴡᴇᴇʙs</a></b>\n' \
-          f'◈ <b>ᴏɴɢᴏɪɴɢ ᴄʜᴀɴɴᴇʟ</b>: <b><a href="https://t.me/Ongoing_Ani_Weebs">ᴏɴɢᴏɪɴɢ ᴡᴇᴇʙs</a></b>\n' \
-          f'◈ <b>Lɪʙʀᴀʀʏ</b>: <b><a href="https://pyrogram.org">Pyʀᴏɢʀᴀᴍ</a></b>\n' \
-          f'◈ <b>ᴍʏ ꜱᴇʀᴠᴇʀ</b>: <b><a href="https://render.com">Rᴇɴᴅᴇʀ</a></b>\n' \
-          f'◈ <b>ᴅᴇᴠᴇʟᴏᴘᴇʀ</b>: <b><a href="https://t.me/voatcb">VØAT</a></b>\n' \
-          f'<b>┗━━━━━━━❪❂❫━━━━━━━━</b>"
+        abt_msg = f'<b>⟦★⟧ Hi There {mention}</b>!💫\n<b>┏━━━━━━━❪❂❫━━━━━━━━</b>\n' \
+                  f'◈ <b>ᴄʀᴇᴀᴛᴏʀ</b>: <b><a href="https://t.me/rai_yan_00">Rᴀɪ Yᴀɴ</a></b>\n' \
+                  f'◈ <b>ꜰᴏᴜɴᴅᴇʀ ᴏꜰ</b>: <b><a href="https://t.me/Ani_Weebs">ᴀɴɪᴍᴇ ᴡᴇᴇʙs</a></b>\n' \
+                  f'◈ <b>ᴏɴɢᴏɪɴɢ ᴄʜᴀɴɴᴇʟ</b>: <b><a href="https://t.me/Ongoing_Ani_Weebs">ᴏɴɢᴏɪɴɢ ᴡᴇᴇʙs</a></b>\n' \
+                  f'◈ <b>Lɪʙʀᴀʀʏ</b>: <b><a href="https://pyrogram.org">Pyʀᴏɢʀᴀᴍ</a></b>\n' \
+                  f'◈ <b>ᴍʏ ꜱᴇʀᴠᴇʀ</b>: <b><a href="https://render.com">Rᴇɴᴅᴇʀ</a></b>\n' \
+                  f'◈ <b>ᴅᴇᴠᴇʟᴏᴘᴇʀ</b>: <b><a href="https://t.me/voatcb">VØAT</a></b>\n' \
+                  f'<b>┗━━━━━━━❪❂❫━━━━━━━━</b>'
         ABOUT_TXT = abt_msg.format(mention=query.from_user.mention)
         await query.message.edit_text(
             text = ABOUT_TXT,
